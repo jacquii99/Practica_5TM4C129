20a412cde7a071ebe1d63b2cca709a9c03f2eeee
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-25 19:32:54 -0600

    frdgchv

--- ADC.c
+++ ADC.c
@@ -17,28 +17,38 @@ extern void Configura_Reg_ADC0(void)
      //Pag 396 para inicializar el modulo de reloj del adc RCGCADC
     SYSCTL->RCGCADC = (1<<0); 
     //Pag 382 (RGCGPIO) Puertos base habilitación del reloj
-    //                     F     E      D       C      B     A
-    SYSCTL->RCGCGPIO |= (1<<5)|(1<<4)|(0<<3)|(0<<2)|(0<<1)|(1<<0)|(1<<12)|(1<<8);
+    //                    
+    SYSCTL->RCGCGPIO |= (0<<0)|(1<<1)|(0<<2)|(1<<3)|(1<<4)|(0<<5)|(0<<6)|(0<<7)|(0<<8)|(0<<9)|(0<<11)|(0<<12);//se habilitan E4 D3 Y B1  
     //Pag 760 (GPIODIR) Habilta los pines como I/O un cero para entrada y un uno para salida
-    GPIOE_AHB->DIR = (0<<5) | (0<<4); //PE5 y PE4
+    GPIOB_AHB->DIR = (0<<5) ; //habilita B5
+    GPIOE_AHB->DIR = (0<<1) |(0<<4); //E1 y E4
+    GPIOD_AHB->DIR = (0<<4) | (0<<5) | (0<<7); //D4 D5 Y D7 
     //(GPIOAFSEL) pag.770 Enable alternate función para que el modulo analógico tenga control de esos pines
-    GPIOE_AHB->AFSEL =  (1<<4) | (1<<5 );
+    GPIOB_AHB->AFSEL = (1<<5):
+    GPIOE_AHB->AFSEL = (1<<1) | (1<<4);
+    GPIOD_AHB->AFSEL = (1<<4) | (1<<5) |(1<<7);
     //(GPIODEN) pag.781 desabilita el modo digital
-    GPIOE_AHB->DEN = (0<<4) | (0<<5 );
+    GPIOB_AHB->DEN = (0<<5);
+    GPIOE_AHB->DEN = (0<<1) | (0<<4);
+    GPIOD_AHB->DEN = (0<<4) | (0<<5) | (0<<7);
     //Pag 787 GPIOPCTL registro combinado con el GPIOAFSEL y la tabla pag 1808
-    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFF00FFFF);
+    GPIOB_AHB->PCTL = GPIOB_AHB->PCTL & (0xFFF0FFFF);
+    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0XFFFF0FF0); 
+    GPIOD_AHB->PCTL = GPIOD_AHB->PCTL & (0XF0F00FFF);
     //(GPIOAMSEL) pag.786 habilitar analogico
-    GPIOE_AHB->AMSEL = (1<<5) | (1<<4);
+    GPIOB_AHB->AMSEL = (1<<5);
+    GPIOE_AHB->AMSEL = (1<<1) | (1<<4); 
+    GPIOD_AHB->AMSEL = (1<<4) | (1<<5) | (1<<7); 
     //Pag 1159 El registro (ADCPC) establece la velocidad de conversión por segundo
-    ADC0->PC = (0<<2)|(0<<1)|(1<<0);//250ksps
+    ADC0->PC = 0X7;//250ksps
     //Pag 1099 Este registro (ADCSSPRI) configura la prioridad de los secuenciadores
-    ADC0->SSPRI = 0x3210;
+    ADC0->SSPRI = 0x0231;
     //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS  =   (0<<3) | (0<<2) | (0<<1) | (0<<0);
+    ADC0->ACTSS  = (0<<2) | (0<<3) | (0<<1);
     //Pag 1091 Este registro (ADCEMUX) selecciona el evento que activa la conversión (trigger)
-    ADC0->EMUX  = (0x0000);
+    ADC0->EMUX  = (0x0000);//
     //Pag 1129 Este registro (ADCSSMUX2) define las entradas analógicas con el canal y secuenciador seleccionado
-    ADC0->SSMUX2 = 0x0089;
+    ADC0->SSMUX2 = 0x0089;//
     //pag 868 Este registro (ADCSSCTL2), configura el bit de control de muestreo y la interrupción
     ADC0->SSCTL2 = (1<<6) | (1<<5) ;
     /* Enable ADC Interrupt */
@@ -61,5 +71,6 @@ extern void ADC0_InSeq2(uint16_t *Result,uint16_t *duty){
        ADC0->ISC = 0x0004;  //Conversion finalizada
 
 }
+extern void Configurar_GPIO
 
 
